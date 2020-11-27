bd50b9ec68e2efb117305d903a60cf9f4cb7124c
Richard Csaba Hafenscher <RichardCsaba.Hafenscher@hu.bosch.com>
2020-11-27 22:58:15 +0100

    forgot in alst commit

--- SWD_HW4/SWD_HW4/MotionBusinnessLogic.cpp
+++ SWD_HW4/SWD_HW4/MotionBusinnessLogic.cpp
@@ -16,96 +16,128 @@ namespace istvan_richard
     {
         if (m_motionCommInfo_r.fGetMovementDetected())
         {
-            if (m_motionCommInfo_r.fGetMovementSize() >= m_motionDetectParam_r.fGetNormalTimeWarningThreshold())
+            if (fIsInValidTime(m_motionDetectParam_r.fGetNormalTimeWarningThreshold()))
             {
-                if (m_motionCommInfo_r.fGetConfidenceInMovement() >= 80
-                    && fIsInValidTime())
+                if (m_motionCommInfo_r.fGetConfidenceInMovement() >= 80 && m_motionCommInfo_r.fGetMovementSize() >= 0.6)
                 {
-                    //Set Normal Warn
-                    m_MotionToSysInfo_r.fUpdateState(CMotion2SysInfo::E_StateOfMotionBussiness::WARNING_IN_TIME);
+                    //Movement in time-frame ->> Granny alive!
+                    m_MotionToSysInfo_r.fUpdateState(CMotion2SysInfo::E_StateOfMotionBussiness::ALL_OK);
                 }
                 else
                 {
-                    //Set SUS
+                    //Potential Movement in time-frame ->> Granny may be alive!
                     m_MotionToSysInfo_r.fUpdateState(CMotion2SysInfo::E_StateOfMotionBussiness::SUS);
                 }
             }
-            else if (m_motionCommInfo_r.fGetMovementSize() >= m_motionDetectParam_r.fGetOutOfNormalTimeWarningThreshold())
+            else
             {
-
-                if (m_motionCommInfo_r.fGetConfidenceInMovement() >= 80
-                    && !fIsInValidTime())
+                if (m_motionCommInfo_r.fGetConfidenceInMovement() >= 80 && m_motionCommInfo_r.fGetMovementSize() >= 0.6)
                 {
-                    //Set Out Of time Warn
-                    m_MotionToSysInfo_r.fUpdateState(CMotion2SysInfo::E_StateOfMotionBussiness::WARNING_OUT_OG_TIME);
+                    //THERE IS AN INTRUDER!!!
+                    m_MotionToSysInfo_r.fUpdateState(CMotion2SysInfo::E_StateOfMotionBussiness::WARNING_OUT_OF_TIME);
                 }
                 else
                 {
-                    //Set Sus
+                    //There might be an intruder!
                     m_MotionToSysInfo_r.fUpdateState(CMotion2SysInfo::E_StateOfMotionBussiness::SUS);
                 }
             }
         }
         else
         {
-            //No movement -> All_OK
-            m_MotionToSysInfo_r.fUpdateState(CMotion2SysInfo::E_StateOfMotionBussiness::ALL_OK);
-        }
-    }
-
-    bool CMotionBusinnessLogic::fIsInValidTime()
-    {
-        bool l_isLaterThanStart_b(false);
-        bool l_isEarlierThanEnd_b(false);
-        for (auto l_it_p = m_motionDetectParam_r.fGetStartOfMonitoring().fGetDaysVector().begin();
-            l_it_p != m_motionDetectParam_r.fGetStartOfMonitoring().fGetDaysVector().begin();
-            l_it_p++)
-        {
-            if (l_it_p->fGetDay() == std::string("ALL")
-                || l_it_p->fGetDay() == m_TimeStamp_r.fGetCurrentTime().fGetDay())
+            // negating value of param, as here we are looking for an outer scope (enlonged timeframe).
+            if (fIsInValidTime((-1)*m_motionDetectParam_r.fGetOutOfNormalTimeWarningThreshold()))
             {
-                if (l_it_p->fGetHour() <= m_TimeStamp_r.fGetCurrentTime().fGetHour()
-                    && l_it_p->fGetMinute() <= m_TimeStamp_r.fGetCurrentTime().fGetMinute()
-                    && l_it_p->fGetSec() <= m_TimeStamp_r.fGetCurrentTime().fGetSec())
-                {
-                    l_isLaterThanStart_b = true;
-                    break;
-                }
-                else
-                {
-                    l_isLaterThanStart_b = false;
-                }
+                    //No Movement in time-fram -> Granny might be in trubble!
+                    m_MotionToSysInfo_r.fUpdateState(CMotion2SysInfo::E_StateOfMotionBussiness::WARNING_IN_TIME);
             }
             else
             {
-                //let for run
+                //There is no intruder in grannys house.
+                m_MotionToSysInfo_r.fUpdateState(CMotion2SysInfo::E_StateOfMotionBussiness::ALL_OK);
             }
         }
+    }
+
+    void CMotionBusinnessLogic::fCheckForMotion()
+    {
+        if (m_motionCommInfo_r.fGetMovementDetected())
+        {
+            if (!m_WasAlreadyMoveDetect_b)
+            {
+                m_WasAlreadyMoveDetect_b = true;
+                m_StartOfMovement = m_TimeStamp_r.fGetCurrentTime();
+            }
+        }
+        else
+        {
+            if (m_WasAlreadyMoveDetect_b)
+            {
+                m_StartOfMovement = { std::string(" "), 0, 0, 0 };
+                m_WasAlreadyMoveDetect_b = false;
+            }
+        }
+    }
+
+    bool CMotionBusinnessLogic::fIsBeforeStartTime(const float& f_thresholdValue_r)
+    {
         for (auto l_it_p = m_motionDetectParam_r.fGetEndOfMonitoring().fGetDaysVector().begin();
-            l_it_p != m_motionDetectParam_r.fGetEndOfMonitoring().fGetDaysVector().begin();
+            l_it_p != m_motionDetectParam_r.fGetEndOfMonitoring().fGetDaysVector().end();
             l_it_p++)
         {
             if (l_it_p->fGetDay() == std::string("ALL")
                 || l_it_p->fGetDay() == m_TimeStamp_r.fGetCurrentTime().fGetDay())
             {
                 if (l_it_p->fGetHour() >= m_TimeStamp_r.fGetCurrentTime().fGetHour()
-                    && l_it_p->fGetMinute() >= m_TimeStamp_r.fGetCurrentTime().fGetMinute()
+                    && l_it_p->fGetMinute() >= (m_TimeStamp_r.fGetCurrentTime().fGetMinute() + f_thresholdValue_r)
                     && l_it_p->fGetSec() >= m_TimeStamp_r.fGetCurrentTime().fGetSec())
                 {
-                    l_isLaterThanStart_b = true;
-                    break;
+                    return true;
                 }
                 else
                 {
-                    l_isLaterThanStart_b = false;
+                    //
                 }
             }
             else
             {
-                //let for run
+                //
             }
         }
-        if (l_isEarlierThanEnd_b && l_isLaterThanStart_b)
+        return false;
+    }
+
+    bool CMotionBusinnessLogic::fIsAfterEndTime(const float& f_thresholdValue_r)
+    {
+        for (auto l_it_p = m_motionDetectParam_r.fGetEndOfMonitoring().fGetDaysVector().begin();
+            l_it_p != m_motionDetectParam_r.fGetEndOfMonitoring().fGetDaysVector().end();
+            l_it_p++)
+        {
+            if (l_it_p->fGetDay() == std::string("ALL")
+                || l_it_p->fGetDay() == m_TimeStamp_r.fGetCurrentTime().fGetDay())
+            {
+                if (l_it_p->fGetHour() <= m_TimeStamp_r.fGetCurrentTime().fGetHour()
+                    && l_it_p->fGetMinute() <= (m_TimeStamp_r.fGetCurrentTime().fGetMinute() - f_thresholdValue_r)
+                    && l_it_p->fGetSec() <= m_TimeStamp_r.fGetCurrentTime().fGetSec())
+                {
+                    return true;
+                }
+                else
+                {
+                    //
+                }
+            }
+            else
+            {
+                //
+            }
+        }
+        return false;
+    }
+
+    bool CMotionBusinnessLogic::fIsInValidTime(const float& f_thresholdValue_r)
+    {
+        if (!fIsAfterEndTime(f_thresholdValue_r) && !fIsBeforeStartTime(f_thresholdValue_r))
         {
             return true;
         }
